3cbd9d44f799c0cf13840590603e2fef64b44bc5
deafmute <96178868+d34fmute@users.noreply.github.com>
2022-11-28 22:27:39 +0100

    Init front (#20)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # qvgds
 Qui Veut Gagner Des Shitcoins
+
+## Front
+
+Run the following commands :
+
+`npm install`
+`npm run dev`
+
+## Back
---
+++ qvgds-front/src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createI18n } from 'vue-i18n'
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const i18n = createI18n({
+  // something vue-i18n options here ...
+});
+
+const app = createApp(App);
+
+app.use(i18n);
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
