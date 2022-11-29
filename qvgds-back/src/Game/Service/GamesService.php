a9d636690ddd616ddcaf90661a0eef67c2c5d1f2
Nolwenn <donolwenn@gmail.com>
2022-11-29 23:37:18 +0100

    Start a game

--- qvgds-back/src/Game/Service/GamesService.php
+++ qvgds-back/src/Game/Service/GamesService.php
@@ -1,7 +1,27 @@
 <?php
 declare(strict_types=1);
 
+namespace QVGDS\Game\Service;
+
+use QVGDS\Game\Domain\Game;
+use QVGDS\Game\Domain\GameId;
+use QVGDS\Session\Domain\SessionId;
+use QVGDS\Session\Domain\SessionNotFoundException;
+use QVGDS\Session\Domain\SessionsRepository;
+
 final class GamesService
 {
+    public function __construct(private readonly SessionsRepository $sessions)
+    {
+    }
 
+    public function start(GameId $id, SessionId $sessionId): Game
+    {
+        $session = $this->sessions->get($sessionId);
+        if ($session === null) {
+            throw new SessionNotFoundException();
+        }
+
+        return Game::start($id, $session);
+    }
 }
