41544a0432aefaa839b108e664097cb14bf9982a
Adrien KISSIE <fredkiss3@gmail.com>
2022-12-03 21:31:46 +0100

    feat: basic tailwind configuration

--- qvgds-front/tailwind.config.cjs
+++ qvgds-front/tailwind.config.cjs
@@ -5,26 +5,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00C966",
-        danger: "#ff005a",
-        secondary: "#928c9e",
-        hollow: {
-          DEFAULT: "#261b3c",
-          light: "#3c2a5f",
+        success: {
+          DEFAULT: "#12C903",
+          50: "#109206"
         },
-        darkest: "#020016",
-        darker: "#0d0a16",
-        dark: "#100a1f",
-      },
-      keyframes: {
-        "ping-sm": {
-          "75%, 100%": { transform: " scale(1.05)", opacity: 0 },
+        danger: {
+          DEFAULT: "#FF3333",
+          50: "#C90F0F"
         },
-      },
-      animation: {
-        "ping-sm": "ping-sm 1.5s ease-in-out infinite",
-      },
-    },
+        warning: {
+          DEFAULT: "#EC8F05",
+          50: "#CD7003"
+        },
+        primary: {
+          DEFAULT: "#4F2FB0"
+        },
+        dark: {
+          DEFAULT: "#291F47",
+          50: "#150D30",
+          40: "#140C2E"
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
