2be5c9bf8a2a2d1d0c576b28a1c01eb6255d4566
Adrien KISSIE <fredkiss3@gmail.com>
2022-12-04 02:12:20 +0100

    update design-system page with the latest components

--- qvgds-front/src/views/DesignSystem.vue
+++ qvgds-front/src/views/DesignSystem.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import PrimaryButton from "@/components/PrimaryButton.vue";
 import ChoiceButton from "@/components/ChoiceButton.vue";
+import JokerButton from "@/components/JokerButton.vue";
+import Tag from "@/components/Tag.vue";
+import WebcamFrame from "@/components/WebcamFrame.vue";
 </script>
 
 <template>
-  <main class="h-screen w-full bg-dark px-10 text-white">
-    <h1 class="text-3xl font-bold">QUI VEUT GAGNER DES SHITCOINS ?</h1>
+  <main
+    class="h-full min-h-screen w-full bg-gradient-to-b from-[#140C2E] to-[#261D45] py-48 px-10 text-white"
+  >
+    <img src="/logo.png" alt="Logo shitcoin" class="mx-auto" />
     <section class="flex flex-col items-center gap-4">
-      <h2 class="text-lg">Réponse</h2>
       <img src="/emojis/poop_emoji.svg" class="h-20 w-20" />
       <PrimaryButton>D&eacute;MARRER</PrimaryButton>
       <PrimaryButton isLoading>DÉMARRER</PrimaryButton>
@@ -18,6 +22,18 @@ import ChoiceButton from "@/components/ChoiceButton.vue";
         <ChoiceButton option="D" label="1994" variant="valid" />
         <ChoiceButton option="C" label="1994" variant="invalid" />
         <ChoiceButton option="C" label="1994" variant="disabled" />
+      </div>
+
+      <div class="flex items-center gap-4">
+        <JokerButton joker="public" />
+        <JokerButton joker="phone" />
+        <JokerButton joker="switch" state="active" />
+        <JokerButton joker="50-50" state="used" />
+      </div>
+
+      <div class="flex flex-col items-start gap-4">
+        <Tag label="JEAN-PIERRE DEAF'" />
+        <WebcamFrame />
       </div>
     </section>
   </main>
