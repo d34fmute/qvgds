27e1a01e93784f5613ec5b365bff84bb450fb768
Adrien KISSIE <fredkiss3@gmail.com>
2022-12-04 01:29:02 +0100

    feat: added tag component

---
+++ qvgds-front/src/components/Tag.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+interface Props {
+  class?: string;
+  label: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  class: ""
+});
+</script>
+
+<template>
+  <span class="rounded-xl bg-primary p-4 font-bold">{{ props.label }}</span>
+</template>
